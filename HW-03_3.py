671e94911c4a36e161c4dcc71e51bddd65a998a8
svitlanakl <sv.klepka@gmail.com>
2024-04-22 21:23:42 +0300

    delete comment to test github

--- HW-03_3.py
+++ HW-03_3.py
@@ -36,5 +36,3 @@ raw_numbers = [
 
 sanitized_numbers = [normalize_phone(num) for num in raw_numbers]
 print("Нормалізовані номери телефонів для SMS-розсилки:", sanitized_numbers)
-
-# check commit to github
